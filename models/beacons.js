2716d515bde80b4de2923d52dc410079a139a2c8
Shane Walsh <walsh.shane@icloud.com>
2017-11-04 16:23:31 +0000

    include references

--- models/beacons.js
+++ models/beacons.js
@@ -1,3 +1,7 @@
+/*
+REF: SSD4 Web App Dev course material lab 2 + 3
+https://ddrohan.github.io/wit-wad/topic02-node/book-b-lab02/index.html#/06
+*/
 var mongoose = require('mongoose');
 
 var BeaconSchema = new mongoose.Schema({
