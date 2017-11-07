7927b37fd757db6a26c02f254a4f4b50c8e5d9a4
Shane Walsh <walsh.shane@icloud.com>
2017-11-07 15:33:03 +0000

    Updated voucher HOOKS

--- test/routes/vouchers-test.js
+++ test/routes/vouchers-test.js
@@ -5,12 +5,16 @@ REF: https://www.sitepoint.com/sinon-tutorial-javascript-testing-mocks-spies-stu
 REF: https://groundberry.github.io/development/2016/12/10/testing-express-with-mocha-and-chai.html
 REF: http://chaijs.com/api/bdd/
 REF: http://mherman.org/blog/2015/09/10/testing-node-js-with-mocha-and-chai/#.Wf3v42-7WRt
+REF: https://mochajs.org
+REF: https://gist.github.com/yoavniran/1e3b0162e1545055429e#mocha
+REF: https://stackoverflow.com/questions/40309713/how-to-send-query-string-parameters-using-supertest
+REF: https://stackoverflow.com/questions/37129668/how-to-write-post-request-to-node-js-server-using-mocha-and-what-are-the-js-need
 */
 
-var vouchers = require('../../models/vouchers');
+var voucher = require('../../models/vouchers');
 var chai = require('chai');
 var chaiHttp = require('chai-http');
-var server = require('../../bin/www');
+var server = require('../../app');
 var expect = chai.expect;
 var _ = require('lodash' );
 var express = require('express');
@@ -22,6 +26,48 @@ chai.use(require('chai-things'));
 
 describe('Vouchers Endpoints', function (){
 
+    beforeEach(function(done){
+        //remove all data from db before each test
+        voucher.remove({},function(err) {
+
+            //create a test instance of Beacon
+            var newVoucher1 = new voucher();
+
+            newVoucher1.active = false;
+            newVoucher1.description = "testdesc1";
+            newVoucher1.value = 100;
+            newVoucher1.type = "testtype1";
+            newVoucher1.save(function (err) {
+                if(err)
+                    console.log("Error Saving to database" + err);
+                else{
+                    //create a 2nd test instance of Beacon
+                    var newVoucher2 = new voucher();
+
+                    newVoucher2.active = true;
+                    newVoucher2.description = "testdesc2";
+                    newVoucher2.value = 200;
+                    newVoucher2.type = "testype2";
+                    newVoucher2.save(function (err) {
+                        if(err)
+                            console.log("Error Saving to database" + err);
+                        else
+                            done();
+                    });
+                }
+            });
+        });
+    });
+    afterEach(function (done) {
+        //Clean db of data after each test
+        voucher.remove({},function(err) {
+
+            if(err)
+                console.log("Error removing data" + err);
+            else
+                done();
+        });
+    });
     describe.only('Get All /vouchers', function () {
         it('should return all vouchers', function(done) {
 
