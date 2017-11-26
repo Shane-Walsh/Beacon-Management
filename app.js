f692042aa12080e03a7a6325d550c92888ebcec8
Shane Walsh <walsh.shane@icloud.com>
2017-11-26 13:22:09 +0000

    TESTING - refactor for webpack bundle

--- app.js
+++ app.js
@@ -27,6 +27,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+//Refactor here for web-pack bundle
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
