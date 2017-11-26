2df56081d40cc750c5fdaa3998ddb4e50b1ad631
Shane Walsh <walsh.shane@icloud.com>
2017-11-26 13:09:36 +0000

    Implemented frontend delete

--- public/javascripts/controllers/beaconcontroller.js
+++ public/javascripts/controllers/beaconcontroller.js
@@ -4,6 +4,9 @@ app.controller('beaconController', ['$scope', '$http',  function($scope, $http) 
     // create a message to display in our view
     $scope.message = 'List all beacons';
 
+    $scope.formData = {};
+    //$scope.newBeacon = [];
+
     findAll();
 
     function findAll() {
@@ -13,6 +16,32 @@ app.controller('beaconController', ['$scope', '$http',  function($scope, $http) 
                 console.log(data);
             })
             .error(function (data) {
+                console.log('Error: ' + data);
+            });
+    };
+    $scope.delete = function(name) {
+        if (confirm("Are you sure you want to delete this Beacon?")){
+            console.log('Deleting Beacon: ' + name);
+            $http.delete('/beacons/' + name)
+                .success(function(data) {
+                    console.log(data);
+                    findAll();
+                })
+                .error(function(data) {
+                    console.log('Error: ' + data);
+                });
+        }
+    };
+    $scope.addBeacon = function(){
+        //$scope.formData.paymenttype = $scope.formData.paymentOptions.name;
+        $http.post('/beacons', $scope.formData)
+            .success(function(data) {
+                $scope.beacons = data;
+                //$location.path('/beacons');
+                console.log(data);
+                findAll();
+            })
+            .error(function(data) {
                 console.log('Error: ' + data);
             });
     };
